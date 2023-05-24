be2130e181c39c5e529642d4eaffbfb21d9d70e7
jinh9015 <jinh9015@gmail.com>
2023-05-24 04:48:36 +0000

    v32

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
                                                                                                                                                                                                       
 @app.route('/')                                                                                    
 def hello():                                                                                       
-    return "Hello COOP Team!\nThis is Jenkins & ArgoCD CI/CI Pipeline test app v31"                                                                        
+    return "Hello COOP Team! \n This is Jenkins & ArgoCD CI/CD Pipeline test app v32"                                                                        
                                                                                                    
 if __name__ == '__main__':                                                                         
     print('start app')     # print문 추가 후 저장                                                  
